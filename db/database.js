73c66afd1667fb129f09a404162472ab86332be9
reiifresh <jreicanete@gmail.com>
2026-06-26 22:04:22 +0800

    Fix syn error: add missing comma in database.js

--- db/database.js
+++ db/database.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
   timezone: '+08:00'  // 👈 THIS CONVERTS ALL TIMES TO PHILIPPINE TIME
 });
 
